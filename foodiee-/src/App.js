bded2817c4e6e4bd57fee9f51b2b06dd1827f663
Varshini11113 <varshini.m2021csec@sece.ac.in>
2024-10-24 12:40:24 +0530

    commited

--- foodiee-/src/App.js
+++ foodiee-/src/App.js
@@ -1,13 +1,14 @@
 import Navbar from "./components/Navbar"
 import HeroSection from "./components/heroSection";
-
+import ImproveSkills from "./components/ImproveSkills";
 function App() {
   return (
     <div className="App">
      <Navbar />
      <div className="container main ">
       <HeroSection/>
-    
+      <ImproveSkills/>
+
      </div>
      
 
--- foodiee-/src/components/heroSection.js
+++ foodiee-/src/components/heroSection.js
@@ -15,14 +15,14 @@ export default function HeroSection()
     ]
     return (
         <div className="section hero ">
-           <div className="col">
+           <div className="col typograpy">
             <h1 className="title"> What Are We About</h1>
             <p classsName="info"> FoodiesHub is a place where you can please your soul and tummy with delicious food recepies of all cuisine. And our service is absolutely free. So start exploring now.</p>
             <button className="btn">explore now</button>
             </div>
             <div className="col gallery">
                 {images.map((src,index)=> (
-                    <CustomImage key={index} imgSrc={src} pt={"85%"}/>
+                    <CustomImage key={index} imgSrc={src} pt={"90%"}/>
                 ))}
                 
                
