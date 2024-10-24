3229ae49925d91dafc3c8b79692c6a6ec4c06057
Varshini11113 <varshini.m2021csec@sece.ac.in>
2024-10-24 10:24:20 +0530

    first commit

---
+++ foodiee-/src/App.js
@@ -0,0 +1,18 @@
+import Navbar from "./components/Navbar"
+import HeroSection from "./components/heroSection";
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+     <div className="container main ">
+      <HeroSection/>
+    
+     </div>
+     
+
+      </div>
+  )
+}
+
+export default App;
